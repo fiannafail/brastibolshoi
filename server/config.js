b865886f38748446fe3da7ecdbdb38f978f2ba34
fiannafail <fiannafail28@gmail.com>
2018-02-02 03:25:47 +0300

    commit

--- server/config.js
+++ server/config.js
@@ -1,8 +1,6 @@
-let REDIS_KEY, JWT_KEY, MONGO_URI
-
-MONGO_URI = 'mongodb://admin:irishlibertian28@46.101.221.53:27017/local'
-JWT_KEY = 'dFTvn3RxVsXE8ZJafmcQSi7fSAL4L9i8'
-REDIS_KEY = 'DRKlPeBHbgr0FUSs'
+const MONGO_URI = 'mongodb://fiannafail:loOhdsdFom5nYCiR@cluster0-shard-00-00-y7wbs.mongodb.net:27017,cluster0-shard-00-01-y7wbs.mongodb.net:27017,cluster0-shard-00-02-y7wbs.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+const JWT_KEY = 'dFTvn3RxVsXE8ZJafmcQSi7fSAL4L9i8'
+const REDIS_KEY = 'DRKlPeBHbgr0FUSs'
 
 export {
 	MONGO_URI,
