14b6b264e82dcd613e0e5c526b2af599c4c79c10
fiannafail <fiannafail28@gmail.com>
2018-02-08 18:01:08 +0300

    commit

--- plugins/global.js
+++ plugins/global.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
-const socket = io(`http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`)
+const socket = io(`http://${process.env.HOST || 'localhost'}:${process.env.PORT || process.env.DOKKU_NGINX_PORT || 3000}`)
 
 export default socket
